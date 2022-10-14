aaff846d34ac727068bc96ac89b996ff60aefb91
Brett Sheleski <brett@sheleski.com>
2022-10-14 14:15:06 -0500

    added new node app for docker image

---
+++ nginx-rtmp/web/index.ts
@@ -0,0 +1,15 @@
+import express, { Application, Request, Response } from 'express'
+
+const app: Application = express()
+
+const port: number = 3001
+
+app.use(express.static('public', {index:false, extensions:['html']}));
+
+app.get('/toto', (req: Request, res: Response) => {
+    res.send('Hello toto')
+})
+
+app.listen(port, function () {
+    console.log(`App is listening on port ${port} !`)
+})
